1062f98f5d742d24ee39e59335954c32d9be8590
Emmanuel <emmanuel.contreras@sw.com.mx>
2018-03-13 17:04:42 -0600

    Test Libreria Ruby

---
+++ UT.rb
@@ -0,0 +1,7 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+require_relative 'C:/Users/SMARTERWEB/Documents/sw-sdk-ruby/SWDSKRuby.rb'
+
+
+puts "Hello World"
