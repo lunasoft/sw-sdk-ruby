385e524932181e66c8054b372af0cc968db2004f
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2023-02-01 13:36:44 -0600

    Add cancellation UR

--- Test/Cancelation/cancelation_test.rb
+++ Test/Cancelation/cancelation_test.rb
@@ -17,7 +17,19 @@ class CancelationTest < Test::Unit::TestCase
         uuid = "14575333-1bf4-4358-9adc-ee8f69ade7d4"
         rfc = "EKU9003173C9"
         motivo = "02"
-        folioSustitucion = ""
+        Cancelation::set(params)
+        response = Cancelation::cancel_csd(uuid, rfc, password_csd, b64_csd, b64_key, motivo)
+        assert(response.get_status == "success", "Expected \"success\" but was #{response.get_status}")
+    end
+    def test_cancelation_csd_sustitucion
+        params = {"url" => 'http://services.test.sw.com.mx', "user" => ENV["SDKTEST_USER"], "password" => ENV["SDKTEST_PASSWORD"]}
+        b64_csd = read_file("resources/b64_csd.txt")
+        b64_key = read_file("resources/b64_key.txt")
+        password_csd = "12345678a"
+        uuid = "3d565100-3518-43f9-9a87-fddb4a1a6453"
+        rfc = "EKU9003173C9"
+        motivo = "01"
+        folioSustitucion = "b02f0526-ad1a-4a0e-a493-17fb0c192f7d"
         Cancelation::set(params)
         response = Cancelation::cancel_csd(uuid, rfc, password_csd, b64_csd, b64_key, motivo, folioSustitucion)
         assert(response.get_status == "success", "Expected \"success\" but was #{response.get_status}")
@@ -29,7 +41,18 @@ class CancelationTest < Test::Unit::TestCase
         uuid = "14575333-1bf4-4358-9adc-ee8f69ade7d4"
         rfc = "EKU9003173C9"
         motivo = "02"
-        folioSustitucion = ""
+        Cancelation::set(params)
+        response = Cancelation::cancel_pfx(uuid, rfc, password_csd, b64_pfx, motivo)
+        assert(response.get_status == "success", "Expected \"success\" but was #{response.get_status}")
+    end
+    def test_Cancelation_pfx_sustitucion
+        params = {"url" => 'http://services.test.sw.com.mx', "user" => ENV["SDKTEST_USER"], "password" => ENV["SDKTEST_PASSWORD"]}
+        b64_pfx = read_file("resources/b64_pfx.txt")
+        password_csd = "12345678a"
+        uuid = "3d565100-3518-43f9-9a87-fddb4a1a6453"
+        rfc = "EKU9003173C9"
+        motivo = "01"
+        folioSustitucion = "b02f0526-ad1a-4a0e-a493-17fb0c192f7d"
         Cancelation::set(params)
         response = Cancelation::cancel_pfx(uuid, rfc, password_csd, b64_pfx, motivo, folioSustitucion)
         assert(response.get_status == "success", "Expected \"success\" but was #{response.get_status}")
@@ -46,7 +69,16 @@ class CancelationTest < Test::Unit::TestCase
         uuid = "14575333-1bf4-4358-9adc-ee8f69ade7d4"
         rfc = "EKU9003173C9"
         motivo = "02"
-        folioSustitucion = ""
+        Cancelation::set(params)
+        response = Cancelation::cancel_uuid(uuid, rfc, motivo)
+        assert(response.get_status == "success", "Expected \"success\" but was #{response.get_status}")
+    end
+    def test_cancelation_uuid_sustitucion
+        params = {"url" => 'http://services.test.sw.com.mx', "user" => ENV["SDKTEST_USER"], "password" => ENV["SDKTEST_PASSWORD"]}
+        uuid = "3d565100-3518-43f9-9a87-fddb4a1a6453"
+        rfc = "EKU9003173C9"
+        motivo = "01"
+        folioSustitucion = "b02f0526-ad1a-4a0e-a493-17fb0c192f7d"
         Cancelation::set(params)
         response = Cancelation::cancel_uuid(uuid, rfc, motivo, folioSustitucion)
         assert(response.get_status == "success", "Expected \"success\" but was #{response.get_status}")
