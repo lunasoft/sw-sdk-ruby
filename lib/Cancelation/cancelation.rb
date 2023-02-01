bd8a08a59e37e1feca3486d941f94ab92812100f
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2023-02-01 13:36:28 -0600

    Set param default value

--- lib/Cancelation/cancelation.rb
+++ lib/Cancelation/cancelation.rb
@@ -6,15 +6,15 @@ class Cancelation < Services
 		Services::set_data(params)
 	end
 
-	def self.cancel_csd(uuid, rfc, password, csd, key, motivo, folioSustitucion)
+	def self.cancel_csd(uuid, rfc, password, csd, key, motivo, folioSustitucion = nil)
 		return SwCancelation::cancel_csd(Services::get_url, Services::get_token, uuid, rfc, password, csd, key, motivo, folioSustitucion)
 	end
 
-	def self.cancel_uuid(uuid, rfc, motivo, folioSustitucion)
+	def self.cancel_uuid(uuid, rfc, motivo, folioSustitucion = nil)
 		return SwCancelation::cancel_uuid(Services::get_url, Services::get_token, uuid, rfc, motivo, folioSustitucion)
 	end
 
-	def self.cancel_pfx(uuid, rfc, password, pfx, motivo, folioSustitucion)
+	def self.cancel_pfx(uuid, rfc, password, pfx, motivo, folioSustitucion = nil)
 		return SwCancelation::cancel_pfx(Services::get_url, Services::get_token, uuid, rfc, password, pfx, motivo, folioSustitucion)
 	end
 
