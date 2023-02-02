075d910584e7910e64f257c5140c83a3c5726f4c
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2023-02-01 22:51:27 -0600

    fix naming

--- lib/Cancelation/cancelation_request.rb
+++ lib/Cancelation/cancelation_request.rb
@@ -4,8 +4,8 @@ require 'json'
 require_relative '../Response/generic_response.rb'
 
 class SwCancelation
-  def self.cancel_uuid(url, token, uuid, rfc, motivo, folioSustitucion)
-    url = URI("#{url}/cfdi33/cancel/#{rfc}/#{uuid}/#{motivo}/#{folioSustitucion}")
+  def self.cancel_uuid(url, token, uuid, rfc, motivo, folio_sustitucion)
+    url = URI("#{url}/cfdi33/cancel/#{rfc}/#{uuid}/#{motivo}/#{folio_sustitucion}")
     http = Net::HTTP.new(url.host, url.port)
     request = Net::HTTP::Post.new(url)
     request["Authorization"] = 'bearer ' + token
@@ -15,28 +15,28 @@ class SwCancelation
     return response_obj.validate_status_code(response_obj)
   end
  
-  def self.cancel_csd(url, token, uuid, rfc, password, csd, key, motivo, folioSustitucion)
+  def self.cancel_csd(url, token, uuid, rfc, password, csd, key, motivo, folio_sustitucion)
     url = URI(url + "/cfdi33/cancel/csd")
     http = Net::HTTP.new(url.host, url.port)
     request = Net::HTTP::Post.new(url)
     request["Content-Type"] = 'application/json'
     request["Authorization"] = 'bearer ' + token
     request["Cache-Control"] = 'no-cache'
-    data = { uuid: uuid, rfc: rfc, b64Cer: csd, b64Key: key, password: password, motivo: motivo, folioSustitucion: folioSustitucion }
+    data = { uuid: uuid, rfc: rfc, b64Cer: csd, b64Key: key, password: password, motivo: motivo, folioSustitucion: folio_sustitucion }
     request.body = data.to_json
     response = http.request(request)
     response_obj = GenericResponse.new(response)
     return response_obj.validate_status_code(response_obj)
   end
 
-  def self.cancel_pfx(url, token, uuid, rfc, password, pfx, motivo, folioSustitucion)
+  def self.cancel_pfx(url, token, uuid, rfc, password, pfx, motivo, folio_sustitucion)
     url = URI(url + "/cfdi33/cancel/pfx")
     http = Net::HTTP.new(url.host, url.port)
     request = Net::HTTP::Post.new(url)
     request["Content-Type"] = 'application/json'
     request["Authorization"] = 'bearer ' + token
     request["Cache-Control"] = 'no-cache'
-    data = { uuid: uuid, rfc: rfc, b64Pfx: pfx, password: password, motivo: motivo, folioSustitucion: folioSustitucion }
+    data = { uuid: uuid, rfc: rfc, b64Pfx: pfx, password: password, motivo: motivo, folioSustitucion: folio_sustitucion }
     request.body = data.to_json
     response = http.request(request)
     response_obj = GenericResponse.new(response)
