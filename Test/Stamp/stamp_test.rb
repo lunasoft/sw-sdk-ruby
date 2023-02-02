42a7225f07969a3ebd7ab9fdc1cb62277e913cdf
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2023-02-02 12:21:09 -0600

    Update Stamp test to use sign helpers

--- Test/Stamp/stamp_test.rb
+++ Test/Stamp/stamp_test.rb
@@ -1,43 +1,49 @@
 require_relative '../../lib/Stamp/stamp.rb'
+require_relative '../test_helper.rb'
 require 'test/unit'
-
-def read_file(file_name)
-    file = File.open(file_name, "r")
-    data = file.read()
-    file.close
-    return data
-end
 
 class StampTest < Test::Unit::TestCase 
     def test_stamp_v1
-        xml = read_file('resources/cfdi40.xml')
+        xml = TestHelper::read_file('resources/cfdi40.xml')
+        pfx = TestHelper::create_pfx
+        password = "12345678a"
+        xml_signed = TestHelper::get_signed_cfdi(xml, pfx, password)
         params = {"url" => 'http://services.test.sw.com.mx', "user" => ENV["SDKTEST_USER"], "password" => ENV["SDKTEST_PASSWORD"]}
         Stamp::set(params)
-        response = Stamp::stamp_v1(xml)
-        assert(response.get_message =~ /^307/, "Expected \"error 307\" but was #{response.get_message}")
+        response = Stamp::stamp_v1(xml_signed)
+        assert(response.get_status == "success", "Expected \"success\" but was #{response.get_status}")
     end
 
     def test_stamp_v2
-        xml = read_file('resources/cfdi40.xml')
+        xml = TestHelper::read_file('resources/cfdi40.xml')
+        pfx = TestHelper::create_pfx
+        password = "12345678a"
+        xml_signed = TestHelper::get_signed_cfdi(xml, pfx, password)
         params = {"url" => 'http://services.test.sw.com.mx', "user" => ENV["SDKTEST_USER"], "password" => ENV["SDKTEST_PASSWORD"]}
         Stamp::set(params)
-        response = Stamp::stamp_v2(xml)
-        assert(response.get_message =~ /^307/, "Expected \"error 307\" but was #{response.get_message}")
+        response = Stamp::stamp_v2(xml_signed)
+        assert(response.get_status == "success", "Expected \"success\" but was #{response.get_status}")
     end
 
     def test_stamp_v3
-        xml = read_file('resources/cfdi40.xml')
+        xml = TestHelper::read_file('resources/cfdi40.xml')
+        pfx = TestHelper::create_pfx
+        password = "12345678a"
+        xml_signed = TestHelper::get_signed_cfdi(xml, pfx, password)
         params = {"url" => 'http://services.test.sw.com.mx', "user" => ENV["SDKTEST_USER"], "password" => ENV["SDKTEST_PASSWORD"]}
         Stamp::set(params)
-        response = Stamp::stamp_v3(xml)
-        assert(response.get_message =~ /^307/, "Expected \"error 307\" but was #{response.get_message}")
+        response = Stamp::stamp_v3(xml_signed)
+        assert(response.get_status == "success", "Expected \"success\" but was #{response.get_status}")
     end
 
     def test_stamp_v4
-        xml = read_file('resources/cfdi40.xml')
+        xml = TestHelper::read_file('resources/cfdi40.xml')
+        pfx = TestHelper::create_pfx
+        password = "12345678a"
+        xml_signed = TestHelper::get_signed_cfdi(xml, pfx, password)
         params = {"url" => 'http://services.test.sw.com.mx', "user" => ENV["SDKTEST_USER"], "password" => ENV["SDKTEST_PASSWORD"]}
         Stamp::set(params)
-        response = Stamp::stamp_v4(xml)
-        assert(response.get_message =~ /^307/, "Expected \"error 307\" but was #{response.get_message}")
+        response = Stamp::stamp_v4(xml_signed)
+        assert(response.get_status == "success", "Expected \"success\" but was #{response.get_status}")
     end
 end
