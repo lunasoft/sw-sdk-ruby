3d0887ea76bff794dcd2b4a9d063d3de15e118ff
NanoCarrillo <fernando.carrillo@sw.com.mx>
2023-10-24 11:55:24 -0600

    Se agrega UT para timbrado de CP30

--- Test/Stamp/stamp_test.rb
+++ Test/Stamp/stamp_test.rb
@@ -53,6 +53,18 @@ class StampTest < Test::Unit::TestCase
     end
 
     def test_stamp_v4_cp30
+        xml = TestHelper::read_file(File.join(__dir__, "../Resources/cfdi/cfdi40_cp30.xml"))
+        pfx = TestHelper::create_pfx
+        password = "12345678a"
+        xml_signed = TestHelper::get_signed_cfdi(xml, pfx, password)
+        params = {"url" => 'http://services.test.sw.com.mx', "user" => ENV["SDKTEST_USER"], "password" => ENV["SDKTEST_PASSWORD"]}
+        Stamp::set(params)
+        response = Stamp::stamp_v4(xml_signed)
+        assert(response.get_status == "success", "Expected \"success\" but was #{response.get_status} - #{response.get_message}")
+        assert(response.get_data != nil?)
+    end
+
+    def test_stamp_v4_cadena_cp30
         xml = TestHelper::read_file(File.join(__dir__, "../Resources/cfdi/cp30.xml"))
         params = {"url" => 'http://services.test.sw.com.mx', "user" => ENV["SDKTEST_USER"], "password" => ENV["SDKTEST_PASSWORD"]}
         Stamp::set(params)
