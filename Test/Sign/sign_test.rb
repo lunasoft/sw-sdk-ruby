aa6f769dea614b432a7f8da123a58393400064e9
ReyesSW <david.reyes@sw.com.mx>
2023-10-24 18:42:13 -0600

    Se actualizan las UT, los path de rutas, ajustes en codigo.

--- Test/Sign/sign_test.rb
+++ Test/Sign/sign_test.rb
@@ -6,14 +6,14 @@ require 'json'
 
 class SignTest < Test::Unit::TestCase
     def test_sign_success
-        xml = TestHelper::read_file('../Resources/cfdi/cfdi40.xml')
-        pfx = TestHelper::create_pfx
-        password = "12345678a"
-        xml_signed = TestHelper::get_signed_cfdi(xml, pfx, password)
-        params = {"url" => 'http://services.test.sw.com.mx', "user" => ENV["SDKTEST_USER"], "password" => ENV["SDKTEST_PASSWORD"]}
-        Stamp::set(params)
-        response = Stamp::stamp_v1(xml_signed)
-        assert(response.get_status == "success", "Expected \"success\" but was #{response.get_status} - #{response.get_message}")
-        assert(response.get_data != nil && !response.get_data.strip.empty)
+      xml = TestHelper::read_file('../Resources/cfdi/cfdi40.xml')
+      pfx = TestHelper::create_pfx
+      password = "12345678a"
+      xml_signed = TestHelper::get_signed_cfdi(xml, pfx, password)
+      params = { "url" => 'http://services.test.sw.com.mx', "user" => ENV["SDKTEST_USER"], "password" => ENV["SDKTEST_PASSWORD"] }
+      Stamp::set(params)
+      response = Stamp::stamp_v1(xml_signed)
+      assert(response.get_status == "success", "Expected \"success\" but was #{response.get_status} - #{response.get_message}")
+      assert(response.get_data.to_s.strip != "") 
     end
-end
+  end
