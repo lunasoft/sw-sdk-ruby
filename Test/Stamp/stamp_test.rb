0de205bf90622661030d97b194e4d41b8cba22b0
Reyes-SW <david.reyes@sw.com.mx>
2024-10-03 11:09:00 -0600

    SS-652_ Actualizar resources y datos para pasar las pruebas.

--- Test/Stamp/stamp_test.rb
+++ Test/Stamp/stamp_test.rb
@@ -1,79 +1,52 @@
-require 'nokogiri'
-require_relative '../../lib/Stamp/stamp.rb'
-require_relative '../../lib/Helpers/sign_helper.rb'
-require_relative '../Helpers/test_helper.rb'
+require_relative '../../lib/Storage/storage.rb'
 require 'test/unit'
 
-class StampTest < Test::Unit::TestCase
-    def test_stamp_v1
-        xml = TestHelper::read_file(File.join(__dir__, "../Resources/cfdi/cfdi40.xml"))
-        pfx = TestHelper::create_pfx
-        password = "12345678a"
-        xml_signed = TestHelper::get_signed_cfdi(xml, pfx, password)
-        params = {"url" => 'http://services.test.sw.com.mx', "user" => ENV["SDKTEST_USER"], "password" => ENV["SDKTEST_PASSWORD"]}
-        Stamp::set(params)
-        response = Stamp::stamp_v1(xml_signed)
-        assert(response.get_status == "success", "Expected \"success\" but was #{response.get_status} - #{response.get_message}")
+class StorageTest < Test::Unit::TestCase 
+    def test_auth_storage_success
+        params = {"url_api" => 'http://api.test.sw.com.mx', "url" => 'http://services.test.sw.com.mx', "user" => ENV["SDKTEST_USER"], "password" => ENV["SDKTEST_PASSWORD"]}
+        Storage::set(params)
+        uuid = "fddb1164-2e76-4022-93ad-fab600eaccdf"
+        response = Storage::get_xml(uuid)
+        assert(response.get_status == "success", "Expected \"success\" but was #{response.get_status}")
         assert(response.get_data != nil?)
+        assert(response.get_data['records'].count > 0)
     end
-
-    def test_stamp_v2
-        xml = TestHelper::read_file(File.join(__dir__, "../Resources/cfdi/cfdi40.xml"))
-        pfx = TestHelper::create_pfx
-        password = "12345678a"
-        xml_signed = TestHelper::get_signed_cfdi(xml, pfx, password)
-        params = {"url" => 'http://services.test.sw.com.mx', "user" => ENV["SDKTEST_USER"], "password" => ENV["SDKTEST_PASSWORD"]}
-        Stamp::set(params)
-        response = Stamp::stamp_v2(xml_signed)
-        assert(response.get_status == "success", "Expected \"success\" but was #{response.get_status} - #{response.get_message}")
+    def test_storage_success
+        params = {"url_api" => 'http://api.test.sw.com.mx', "token" => ENV["SDKTEST_TOKEN"]}
+        Storage::set(params)
+        uuid = "fddb1164-2e76-4022-93ad-fab600eaccdf"
+        response = Storage::get_xml(uuid)
+        assert(response.get_status == "success", "Expected \"success\" but was #{response.get_status}")
         assert(response.get_data != nil?)
+        assert(response.get_data['records'].count > 0)
     end
-
-    def test_stamp_v3
-        xml = TestHelper::read_file(File.join(__dir__, "../Resources/cfdi/cfdi40.xml"))
-        pfx = TestHelper::create_pfx
-        password = "12345678a"
-        xml_signed = TestHelper::get_signed_cfdi(xml, pfx, password)
-        params = {"url" => 'http://services.test.sw.com.mx', "user" => ENV["SDKTEST_USER"], "password" => ENV["SDKTEST_PASSWORD"]}
-        Stamp::set(params)
-        response = Stamp::stamp_v3(xml_signed)
-        assert(response.get_status == "success", "Expected \"success\" but was #{response.get_status} - #{response.get_message}")
+    def test_storage_invalid_auth
+        begin
+        params = {"url_api" => 'http://api.test.sw.com.mx'}
+        Storage::set(params)
+        uuid = "fddb1164-2e76-4022-93ad-fab600eaccdf"
+        response = Storage::get_xml(uuid)
+        rescue RuntimeError => e
+            assert(e.message == "Datos de autenticación deben especificarse", "Response was #{e.message}")
+        end
+    end
+    def test_storage_invalid_url
+        begin
+            params = {"urlAPI" => 'http://api.test.sw.com.mx', "token" => ENV["SDKTEST_TOKEN"]}
+            Storage::set(params)
+            uuid = "fddb1164-2e76-4022-93ad-fab600eaccdf"
+            response = Storage::get_xml(uuid)
+        rescue RuntimeError => e
+            assert(e.message == "URL debe especificarse", "Response was #{e.message}")
+        end
+    end
+    def test_storage_norecords
+        params = {"url_api" => 'http://api.test.sw.com.mx', "token" => ENV["SDKTEST_TOKEN"]}
+        Storage::set(params)
+        uuid = "b35d525e-d845-42c9-bbfb-eeeef601e2b4"
+        response = Storage::get_xml(uuid)
+        assert(response.get_status == "success", "Expected \"success\" but was #{response.get_status}")
         assert(response.get_data != nil?)
-    end
-
-    def test_stamp_v4
-        xml = TestHelper::read_file(File.join(__dir__, "../Resources/cfdi/cfdi40.xml"))
-        pfx = TestHelper::create_pfx
-        password = "12345678a"
-        xml_signed = TestHelper::get_signed_cfdi(xml, pfx, password)
-        params = {"url" => 'http://services.test.sw.com.mx', "user" => ENV["SDKTEST_USER"], "password" => ENV["SDKTEST_PASSWORD"]}
-        Stamp::set(params)
-        response = Stamp::stamp_v4(xml_signed)
-        assert(response.get_status == "success", "Expected \"success\" but was #{response.get_status} - #{response.get_message}")
-        assert(response.get_data != nil?)
-    end
-
-    def test_stamp_v4_cp30
-        xml = TestHelper::read_file(File.join(__dir__, "../Resources/cfdi/cfdi40_cp30.xml"))
-        pfx = TestHelper::create_pfx
-        password = "12345678a"
-        xml_signed = TestHelper::get_signed_cfdi(xml, pfx, password)
-        params = {"url" => 'http://services.test.sw.com.mx', "user" => ENV["SDKTEST_USER"], "password" => ENV["SDKTEST_PASSWORD"]}
-        Stamp::set(params)
-        response = Stamp::stamp_v4(xml_signed)
-        assert(response.get_status == "success", "Expected \"success\" but was #{response.get_status} - #{response.get_message}")
-        assert(response.get_data != nil?)
-    end
-
-    def test_stamp_v4_cadena_cp30
-        xml = TestHelper::read_file(File.join(__dir__, "../Resources/cfdi/cp30.xml"))
-        params = {"url" => 'http://services.test.sw.com.mx', "user" => ENV["SDKTEST_USER"], "password" => ENV["SDKTEST_PASSWORD"]}
-        Stamp::set(params)
-        response = Stamp::stamp_v4(xml)
-        cadena = SignHelper::get_cadena_original(xml)
-        cadena_esperada = cadena.to_xml.gsub(/<\?xml[^>]+\?>|\n/, '')
-        cadena_original = "||4.0|Serie|Folio|2023-10-02T11:30:55|01|30001000000500003416|100.00|MXN|100.00|I|01|PUE|20000|EKU9003173C9|ESCUELA KEMPER URGATE|601|URE180429TM6|UNIVERSIDAD ROBOTICA ESPAÑOLA|86991|601|G01|78101800|UT421511|1|H87|Pieza|Transporte de carga por carretera|100.00|100.00|01|3.0|CCC98765-FEDC-5678-4321-ABCDEF098765|No|5|Origen|OR123456|URE180429TM6|2023-10-02T18:00:00|2109|05|057|OAX|MEX|70300|Destino|DE123456|URE180429TM6|2023-10-02T19:00:00|5|2109|05|057|OAX|MEX|70300|1|KGM|1|10101500|Mercancia|1.00|18|01|Medicamento|Medicamento|SW Sapien|2024-01-23|123456|01|03|123456789|1.00|1|OR123456|DE123456|TPAF01|12345|C2|100|ABC123|2023|SW Sapien|12345|01|123456789|Nombre Apellido||"
-        assert(response.get_message =~ /^307/, "Expected \"success\" but was #{response.get_status}")
-        assert(cadena_esperada == cadena_original)
+        assert(response.get_data['records'].count == 0)
     end
 end
