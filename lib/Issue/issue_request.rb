52e380db0e8551b6caf7e1b68d8c56802685d155
Fernando Carrillo <108951984+FerCarrilloM@users.noreply.github.com>
2025-06-30 12:10:17 -0600

    Se actualizan los request para hacer uso del Helper.

--- lib/Issue/issue_request.rb
+++ lib/Issue/issue_request.rb
@@ -1,36 +1,21 @@
-require 'net/http'
-require 'json'
-require 'securerandom'
 require_relative '../Response/generic_response.rb'
+require_relative '../Helpers/request_helper.rb'
 
 class SwIssue
   def self.issue(url, token, version, xml, base64)
     b64 = ""
     if base64
-      b64 = '/b64/' 
+      b64 = '/b64/'
     end
-    boundary =  '----' + SecureRandom.urlsafe_base64
-    url = URI(url + "/cfdi33/issue/" + version + b64)
-    http = Net::HTTP.new(url.host, url.port)
-    request = Net::HTTP::Post.new(url)
-    request["Authorization"] = 'bearer ' + token
-    request["Content-Type"] = 'multipart/form-data; boundary="' + boundary + '"'
-    request["Cache-Control"] = 'no-cache'
-    request.body = "--" + boundary + "\r\nContent-Type: text/xml\r\nContent-Transfer-Encoding: binary\r\nContent-Disposition: form-data; name=\"xml\"; filename=\"xml\"\r\n\r\n" + xml + "\r\n--" + boundary + "--"
-    response = http.request(request)
+    endpoint = URI(url + "/cfdi33/issue/" + version + b64)
+    response = RequestHelper.post_multipart_request(endpoint,token,xml)
     response_obj = GenericResponse.new(response)
     return response_obj.validate_status_code(response_obj)
   end
 
   def self.issue_JSON(url, token, version, json)
-    url = URI(url + "/v3/cfdi33/issue/json/" + version)
-    http = Net::HTTP.new(url.host, url.port)
-    request = Net::HTTP::Post.new(url)
-    request["Authorization"] = 'bearer ' + token
-    request["Content-Type"] = 'application/jsontoxml'
-    request["Cache-Control"] = 'no-cache'
-    request.body = json
-    response = http.request(request)
+    endpoint = URI(url + "/v3/cfdi33/issue/json/" + version)
+    response = RequestHelper.post_jsontoxml_request(endpoint,token,json)
     response_obj = GenericResponse.new(response)
     return response_obj.validate_status_code(response_obj)
   end
--- lib/StatusCfdi/status_cfdi_request.rb
+++ lib/StatusCfdi/status_cfdi_request.rb
@@ -3,28 +3,32 @@ require 'net/https'
 require_relative 'status_cfdi_response.rb'
 
 class SwStatusCfdi
-    def self.status(url, action, rfc_emisor, rfc_receptor, total,uuid)
-        data = <<-EOF
-        <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tem="http://tempuri.org/">
-        <soapenv:Header/>
-        <soapenv:Body>
-           <tem:Consulta>
-              <tem:expresionImpresa><![CDATA[?re=#{rfc_emisor.encode(:xml => :text)}&rr=#{rfc_receptor.encode(:xml => :text)}&tt=#{total}&id=#{uuid}]]></tem:expresionImpresa>
-           </tem:Consulta>
-        </soapenv:Body>
-     </soapenv:Envelope>
+  def self.status(url, action, rfc_emisor, rfc_receptor, total, uuid)
+    data = <<-EOF
+    <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tem="http://tempuri.org/">
+      <soapenv:Header/>
+      <soapenv:Body>
+        <tem:Consulta>
+          <tem:expresionImpresa><![CDATA[?re=#{rfc_emisor.encode(xml: :text)}&rr=#{rfc_receptor.encode(xml: :text)}&tt=#{total}&id=#{uuid}]]></tem:expresionImpresa>
+        </tem:Consulta>
+      </soapenv:Body>
+    </soapenv:Envelope>
     EOF
 
-    url = URI(url)
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    request = Net::HTTP::Post.new(url)
-    request["Content-Type"] = 'text/xml; charset=utf-8'
-    request["SOAPAction"] = action
+    uri = URI(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = uri.scheme == 'https'
+    http.ssl_version = :TLSv1_2
+    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+
+    request = Net::HTTP::Post.new(uri)
+    request['Content-Type'] = 'text/xml; charset=utf-8'
+    request['SOAPAction'] = action
     request.body = data
+
     response = http.request(request)
-    response.body.force_encoding("UTF-8")
-    return StatusCfdiResponse.new(response)
-    end
+    response.body.force_encoding('UTF-8')
+
+    StatusCfdiResponse.new(response)
+  end
 end
